bcbdb258767ab5b54dbaa21a01b386178abcb254
Tvisher <tvisherfine@gmail.com>
2026-07-23 18:08:41 +0500

    !

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-const firstScreenBg = document.querySelector('.first-screen__bg')
+const firstScreenBg = document.querySelector('.first-screen__bg');
 
+const changeImage = (sliderEl) => {
+    const currentSlide = sliderEl.slides[sliderEl.activeIndex];
+    if (currentSlide) {
+        const slideImage = currentSlide.querySelector('img');
+        if (slideImage) {
+            const imagePath = slideImage.getAttribute('src')
+            firstScreenBg.style.backgroundImage = `url(${imagePath})`;
+        }
+    }
+
+}
 
 const mainSlider = new Swiper('.first-screen__slider', {
     spaceBetween: 40,
     speed: 800,
     on: {
         init(swiper) {
-            const currentSlide = swiper.slides[swiper.activeIndex];
-            const slideImage = currentSlide.querySelector('img').src
-            firstScreenBg.style.backgroundImage = `url(${slideImage})`;
+            changeImage(swiper)
         },
         activeIndexChange(swiper) {
-            const currentSlide = swiper.slides[swiper.activeIndex];
-            const slideImage = currentSlide.querySelector('img').src
-            firstScreenBg.style.backgroundImage = `url(${slideImage})`;
+            changeImage(swiper)
         }
     }
 })
