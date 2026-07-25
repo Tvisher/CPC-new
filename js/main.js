daa4971cc528b623062f586539a17d590e6309c4
Tvisher <tvisherfine@gmail.com>
2026-07-25 19:48:26 +0500

    !

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const firstScreenBg = document.querySelector('.first-screen__bg');
-
 const changeImage = (sliderEl) => {
     const currentSlide = sliderEl.slides[sliderEl.activeIndex];
     if (currentSlide) {
@@ -15,9 +14,11 @@ const changeImage = (sliderEl) => {
 const mainSlider = new Swiper('.first-screen__slider', {
     spaceBetween: 40,
     speed: 800,
+    // loop: 1,
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
+        clickable: 1
     },
     on: {
         init(swiper) {
@@ -27,33 +28,54 @@ const mainSlider = new Swiper('.first-screen__slider', {
             changeImage(swiper)
         }
     }
+});
+const leftMenuInner = document.querySelector('.left-menu__content');
+const bodyEl = document.querySelector('body');
+const leftMenu = document.querySelector('.left-menu');
+document.addEventListener('click', e => {
+    const target = e.target;
+    if (bodyEl.classList.contains('menu-open') && !target.closest('.left-menu__content')) {
+        leftMenu.classList.remove('open');
+        leftMenuInner.classList.remove('open');
+        bodyEl.classList.remove('menu-open');
+        return
+    }
+    if (target.closest('.left-menu')) {
+        leftMenu.classList.toggle('open');
+        leftMenuInner.classList.toggle('open');
+        bodyEl.classList.toggle('menu-open');
+        return
+    }
+
+    if (target.closest('.next-arrow')) {
+        mainSlider.slideNext(800)
+    }
+    if (target.closest('.prev-arrow')) {
+        mainSlider.slidePrev(800)
+    }
+
 })
 
 
-// const header = document.querySelector('header.header');
-// const fixedHeader = document.querySelector('.fixed-header');
-
-// const checkWindowScroll = () => {
-//     if (window.scrollY > header.clientHeight) {
-//         fixedHeader.classList.add('show')
-//     } else {
-//         fixedHeader.classList.remove('show')
-//     }
-// }
-// checkWindowScroll()
-// document.addEventListener('scroll', (e) => checkWindowScroll())
 
 const header = document.querySelector('header.header');
 const fixedHeader = document.querySelector('.fixed-header');
-
 if (header && fixedHeader) {
     const observer = new IntersectionObserver(
         ([entry]) => {
-            // Покажем fixedHeader, когда header полностью ушел с экрана
             fixedHeader.classList.toggle('show', !entry.isIntersecting);
         },
-        { threshold: 0 } // Реакция сразу при выходе элемента из Viewport
+        { threshold: 0 }
     );
 
     observer.observe(header);
 }
+
+
+
+
+$(".nav-menu__item .open-arrow").on("click", function () {
+    const innerMenu = $(this).parents('.nav-menu__item').find('.nav-menu__submenu-wrapper');
+    $(this).toggleClass('open')
+    innerMenu.slideToggle("slow", function () { });
+});
